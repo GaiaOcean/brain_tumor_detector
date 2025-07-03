9ddacf87ca0f610035c73668a29027ef9cd41c5a
Grazielle <grazibatista2005@gmail.com>
2025-07-03 12:22:50 -0300

    Fixed Bugs in the grad_cam file and model_testing

--- grad_cam.py
+++ grad_cam.py
@@ -15,34 +15,37 @@ model = keras.models.load_model('tumor_detector.h5')
 def get_img_path(img_path: str) -> str:
     return img_path
 
-def normalize_img(img_path: str) -> str:
-    if img_path.split('.')[1] == 'jpg':
-        image = cv2.imread(img_path)
-        image = Image.fromarray(image)
-        image = image.resize((64,64))
-        image = np.expand_dims(image,axis=0)
-
-        return image
-    else:
-        raise ValueError("Unsupported image format. Convert the image to jpg")
+def normalize_img(img_path: str) -> np.ndarray:
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # optional: convert BGR to RGB
+    image = Image.fromarray(image)
+    image = image.resize((64, 64))
+    image = np.asarray(image).astype(np.float32) / 255.0  # normalize and convert dtype
+    image = np.expand_dims(image, axis=0)
+    return image
 
 #gets the raw output of the model throught the paramiter and outputs tensor of floating-point numbers
 def get_score(model_output):
-    return model_output[:,0]
+    return model_output[0]
 
-def grad_cam_generator(model,model_output,image):
-    score = get_score(model_output)
+
+def grad_cam_generator(model,image):
     linear = ReplaceToLinear()
     gradcam = Gradcam(model, model_modifier= linear, clone=True)
-    cam = gradcam(score, image, penultimate_layer=-1) 
+    cam = gradcam(get_score, image, penultimate_layer=-1) 
     heatmap = np.uint8(255 * cam[0])
     img = image[0]
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    superimposed_img = heatmap * 0.4 + (img * 255).astype(np.uint8)
+    heatmap_img = heatmap * 0.4 + (img * 255).astype(np.uint8)
+    model_output = model.predict(image)
+    score = get_score(model_output)
 
-    plt.imshow(superimposed_img.astype('uint8'))
+    plt.imshow(heatmap_img.astype('uint8'))
     plt.axis('off')
-    plt.title('Brain Scan')
+    if score >= 0.5:
+        plt.title("Tumor detected", fontsize=14, color='red')
+    else:
+        plt.title("TNo tumor detected", fontsize=14, color='green')
     plt.show()
     
-    return superimposed_img
+    return heatmap_img
--- model_testing.py
+++ model_testing.py
@@ -2,14 +2,21 @@ import cv2
 from PIL import Image
 from keras.models import load_model
 import numpy as np
-
+from grad_cam import grad_cam_generator, normalize_img, get_score
 model = load_model('tumor_detector.h5')
 
-val_img1 = cv2.imread('C:\\Users\\User\\Downloads\\tumor_dector\\dataset\\pred\\pred56.jpg')
+img_path = 'C:\\Users\\User\\Downloads\\tumor_dector\\dataset\\pred\\pred58.jpg'
+input_img = normalize_img(img_path)
 
-img = Image.fromarray(val_img1)
-img = img.resize((64,64))
-img = np.array(img)
-img = np.expand_dims(img,axis=0)
-pred = model.predict(img)
-print(pred)
+
+heatmap_img = grad_cam_generator(model,input_img)
+
+
+
+
+# img = Image.fromarray(val_img1)
+# img = img.resize((64,64))
+# img = np.array(img)
+# img = np.expand_dims(img,axis=0)
+# pred = model.predict(img)
+# print(pred)
